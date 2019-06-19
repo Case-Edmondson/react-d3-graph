d7444d36672bbb52cc8cf12d3479c1c111df2b5f
Case-Edmondson <51298996+Case-Edmondson@users.noreply.github.com>
2019-06-19 12:59:33 -0500

    Update node.const.js

--- src/components/node/node.const.js
+++ src/components/node/node.const.js
@@ -7,7 +7,7 @@ export default {
         END_ANGLE: 2 * Math.PI,
     },
     DEFAULT_NODE_SIZE: CONFIG.node.size,
-    NODE_LABEL_DX: ".90em",
+    NODE_LABEL_DX: "0em",
     NODE_LABEL_DY: ".35em",
     ...CONST,
 };
